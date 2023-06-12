34bf70f841ba69df6175ccd7623ca9fc61bf8c10
j2seon <ddj04323@gmail.com>
2023-06-12 15:46:32 +0900

    20230612

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -11,6 +11,6 @@ new Swiper(".mySwiper", {
 new Swiper('.swiper2', {
 	// loop : true, // 무한 루프 슬라이드, 반복이 되며 슬라이드가 끝이 없다.
 	pagination : {
-		// el : '.swiper-pagination',
+		el : '.swiper-pagination',
 	},
 });
